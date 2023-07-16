fb732024a4a70f13721e8c4ed5a7240e63316c64
ahmed hamada <ahmedhamada496@gmail.com>
2023-07-16 06:00:38 +0300

    build functionality to draw plots and upgrade the validation

--- app.py
+++ app.py
@@ -1,35 +1,30 @@
+import sys
+import re
+import math
+import numpy as np
 from PySide6.QtWidgets import (
     QApplication, QLabel, QLineEdit, QPushButton,
     QVBoxLayout, QHBoxLayout, QWidget, QMessageBox
 )
-from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import re
-import math
 
 
 # list of allowed words to be entered by the user
-ALLOWED_WORDS = ["sin", "cos", "tan", "exp", "+", "-", "*", "/", "x"]
+ALLOWED_FUNCTIONS = ["sin", "cos", "tan", "exp",
+                     "log", "sqrt", "+", "-", "*", "/", "^", "x"]
 
 # message format
 message = f"I'm sorry, any variable else 'x' is not allowed in your math expression. \n\n"
 message += f"Please enter a function of 'x' only. For example: \n"
 message += "  - 5*x^3 + 2/x - 1\n"
 message += "\nYou can use these math symbols:\n"
-message += f" - Functions: {', '.join(ALLOWED_WORDS[:-4])}\n"
-message += f" - Operators: {', '.join(ALLOWED_WORDS[-4:])}\n\n"
+message += f" - Functions: {', '.join(ALLOWED_FUNCTIONS[:-4])}\n"
+message += f" - Operators: {', '.join(ALLOWED_FUNCTIONS[-4:])}\n\n"
 message += "Please update your expression to use only the allowed variable and functions."
-
-replacements = {
-    'sin': 'np.sin',
-    'cos': 'np.cos',
-    'exp': 'np.exp',
-    'sqrt': 'np.sqrt',
-    '^': '**', }
 
 
 class MainWindow(QWidget):
-
     def __init__(self):
         super().__init__()
 
@@ -41,14 +36,15 @@ class MainWindow(QWidget):
         # define figure or chart GUI
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
+        self.axes = self.figure.subplots()
 
         # Create widgets
         self.function_label = QLabel("Enter function: y = ")
         self.function_input = QLineEdit()
-        self.minx_label = QLabel("Min x:")
-        self.minx_input = QLineEdit()
-        self.maxx_label = QLabel("Max x:")
-        self.maxx_input = QLineEdit()
+        self.min_x_label = QLabel("Min x:")
+        self.min_x_input = QLineEdit()
+        self.max_x_label = QLabel("Max x:")
+        self.max_x_input = QLineEdit()
         self.submit_button = QPushButton("Plot")
         # define error dialog
         self.error_dialog = QMessageBox()
@@ -59,44 +55,76 @@ class MainWindow(QWidget):
         function_layout.addWidget(self.function_input)
 
         xrange_layout = QHBoxLayout()
-        xrange_layout.addWidget(self.minx_label)
-        xrange_layout.addWidget(self.minx_input)
-        xrange_layout.addWidget(self.maxx_label)
-        xrange_layout.addWidget(self.maxx_input)
+        xrange_layout.addWidget(self.min_x_label)
+        xrange_layout.addWidget(self.min_x_input)
+        xrange_layout.addWidget(self.max_x_label)
+        xrange_layout.addWidget(self.max_x_input)
 
         self.layout.addLayout(function_layout)
         self.layout.addLayout(xrange_layout)
         self.layout.addWidget(self.submit_button)
         self.layout.addWidget(self.canvas)
 
-        self.submit_button.clicked.connect(lambda _: self.plot_btn_submitted())
+        self.submit_button.clicked.connect(self.plot_btn_submitted)
+        self.function_input.textChanged.connect(self.handle_function_changed)
+        self.min_x_input.textChanged.connect(self.handle_function_changed)
+        self.max_x_input.textChanged.connect(self.handle_function_changed)
 
     def plot_btn_submitted(self):
         try:
-            print(self.function_input.text())
-            y = func_2_string(self.function_input.text())
+            fn = self.function_input.text()
+            mn = self.min_x_input.text()
+            mx = self.max_x_input.text()
+            validate_inputs(fn, mn, mx)
+            self.plot_function(fn, mn, mx)
+
         except ValueError as e:
             self.error_dialog.setWindowTitle("Error!")
             self.error_dialog.setText(str(e))
             self.error_dialog.show()
         return
 
+    def handle_function_changed(self, text):
+        """Handle function text changed"""
+        try:
+            fn = self.function_input.text()
+            mn = self.min_x_input.text()
+            mx = self.max_x_input.text()
+            validate_inputs(fn, mn, mx)
+            self.plot_function(fn, mn, mx)
+        except:
+            pass
+
+    def plot_function(self, fn_text, fn_min, fn_max):
+        """Plot the function"""
+        fn_max = float(fn_max)
+        fn_min = float(fn_min)
+        x = np.linspace(fn_min, fn_max, 1000)
+        y = func_2_string(fn_text)(x)
+        self.axes.clear()
+        self.axes.plot(x, y)
+        self.canvas.draw()
+
 
 def validate_expression(expression):
     """Check that expression contains only allowed words and symbols"""
     for word in re.findall(r"[a-zA-Z_]+", expression):
-        if word not in ALLOWED_WORDS:
+        if word not in ALLOWED_FUNCTIONS:
             raise ValueError(message)
 
 
 def substitute_constants(expression):
     """Replace predefined constant values"""
-    constants = {"pi": str(math.pi),
-                 'sin': 'np.sin',
-                 'cos': 'np.cos',
-                 'exp': 'np.exp',
-                 'sqrt': 'np.sqrt',
-                 '^': '**'}
+    constants = {
+        "pi": str(math.pi),
+        "sin": "np.sin",
+        "cos": "np.cos",
+        'tan': 'np.tan',
+        "exp": "np.exp",
+        "sqrt": "np.sqrt",
+        'log': 'np.log',
+        "^": "**",
+    }
     for old, new in constants.items():
         expression = expression.replace(old, new)
     return expression
@@ -114,16 +142,40 @@ def func_2_string(expression):
     # Append x*0 to handle constant expressions
     if "x" not in expression:
         expression += "+0*x"
-    print(expression)
 
-    def func():
+    def func(x):
         return eval(expression)
 
-    return func
+    return np.vectorize(func)
+
+
+def validate_inputs(function, min_x, max_x):
+    # Validate function
+    if not function.strip():
+        raise ValueError("Please enter a function to plot")
+
+    # Validate range inputs
+    if not min_x or not max_x:
+        raise ValueError("Range values cannot be empty")
+
+    if not re.fullmatch(r"[-+]?\d*\.?\d+", min_x) or not re.fullmatch(r"[-+]?\d*\.?\d+", max_x):
+        raise ValueError("Range inputs must be numeric")
+
+    min_x = float(min_x)
+    max_x = float(max_x)
+
+    if min_x > max_x:
+        raise ValueError("Minimum value must be less than maximum")
+
+    # Function validation
+    try:
+        func_2_string(function)
+    except Exception as e:
+        raise ValueError("Invalid function") from e
 
 
 if __name__ == "__main__":
     app = QApplication([])
     widget = MainWindow()
     widget.show()
-    app.exec()
+    sys.exit(app.exec())
